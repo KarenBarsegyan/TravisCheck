dc416088e3fd4167605c6ab728eda06dc13e8536
KarenBarsegyan <karen.barsegyan-2001@bk.ru>
2019-11-30 13:28:20 +0300

    triang

---
+++ triangulation/src/teleop_recieve_send.cpp
@@ -0,0 +1,44 @@
+#include "ros/ros.h"
+#include "geometry_msgs/Twist.h"
+#include "fsd_common_msgs/ControlCommand.h"
+
+fsd_common_msgs::ControlCommand Com;
+
+void scanCallBack(const geometry_msgs::Twist msg) {
+    if (msg.linear.x > 0 && Com.throttle.data < 1)
+        Com.throttle.data += 0.05;
+    else if (msg.linear.x < 0 && Com.throttle.data >= 0)
+        Com.throttle.data -= 0.05;
+
+    if (msg.angular.z > 0)
+        Com.steering_angle.data = 0.2;
+    else if (msg.angular.z < 0)
+        Com.steering_angle.data = -0.2;
+    else
+        Com.steering_angle.data = 0;
+}
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "teleop_recieve_send");
+    ros::NodeHandle scan;
+    ros::NodeHandle pub;
+
+    ros::Subscriber teleop_scan = scan.subscribe("turtle1/cmd_vel", 1000, scanCallBack);
+
+    ros::Publisher
+        teleop_pub = pub.advertise<fsd_common_msgs::ControlCommand>(
+            "/control/pure_pursuit/control_command", 0);
+
+    ros::Rate rate(10);
+
+    while (ros::ok()) {
+        teleop_pub.publish(Com);
+
+        Com.steering_angle.data = 0;
+
+        ros::spinOnce();
+        rate.sleep();
+    }
+
+    return 0;
+}
